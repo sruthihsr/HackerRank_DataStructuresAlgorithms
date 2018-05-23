311b4f3eb127680748ce9c23f3e3bf60c09bd6e5
sruthihsr <sruthihsr@gmail.com>
2018-05-23 15:46:20 -0700

    Create Arrays.cpp
    initial commit

---
+++ Arrays_LeftRotation/Arrays.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the solve function below.
+vector<int> solve(vector<int> a, int d) {
+vector<int> b=a;
+
+    for (int i=0;i<a.size();i++)
+    {
+        signed int new_iter=i-d;
+        cout<<"val="<<new_iter<<endl;
+        if(new_iter<0)
+        {
+         new_iter =(a.size() + new_iter) ;
+        }
+        cout<<"new_ter"<<new_iter<<endl;
+        b[new_iter]=a[i];
+        
+        
+    }
+    for (vector<int>::iterator it = b.begin() ; it != b.end(); ++it)
+            cout <<' '<< *it;   
+ return b;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nd_temp;
+    getline(cin, nd_temp);
+
+    vector<string> nd = split_string(nd_temp);
+
+    int n = stoi(nd[0]);
+
+    int d = stoi(nd[1]);
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    vector<int> result = solve(a, d);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
